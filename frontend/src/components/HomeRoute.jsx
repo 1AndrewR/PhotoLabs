9e84419a012ce4063bbaf5ecaeaf6a03acd05046
Andrew Roth <a.roth.80@hotmail.com>
2025-03-11 16:32:30 -0700

    Modal created

--- frontend/src/components/HomeRoute.jsx
+++ frontend/src/components/HomeRoute.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import TopNavigationBar from './TopNavigationBar';
-import PhotoList from './PhotoList';
+import PhotoList from '../components/PhotoList';
 import '../styles/HomeRoute.scss';
 
-const HomeRoute = ({ photos, topics, openModal }) => (
-  <div className="home-route">
-    <TopNavigationBar topics={topics} />
-    <PhotoList photos={photos} openModal={openModal} />
-  </div>
-);
+const HomeRoute = ({ photos, openModal }) => {
+  return (
+    <div className="home-route">
+      <PhotoList photos={photos} openModal={openModal} />
+    </div>
+  );
+};
 
 export default HomeRoute;
