f5dc3a48845437eacf6664ff0e21ad22733f9121
Andrew Roth <a.roth.80@hotmail.com>
2025-03-11 10:40:26 -0700

    Now showing 3 pictures

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,32 +2,69 @@ import React from 'react';
 import PhotoListItem from './components/PhotoListItem';
 import './App.scss';
 
-const sampleDataForPhotoListItem = {
-  id: 1,
-  location: {
-    city: "Montreal",
-    country: "Canada",
+const photoData = [
+  {
+    id: 1,
+    location: {
+      city: "Montreal",
+      country: "Canada",
+    },
+    urls: {
+      full: "/Image-1-Full.jpeg",
+      regular: "/Image-1-Regular.jpeg",
+    },
+    user: {
+      username: "exampleuser1",
+      name: "Joe Example",
+      profile: "/profile-1.jpg",
+    },
   },
-  urls: {
-    full: "/Image-1-Full.jpeg",
-    regular: "/Image-1-Regular.jpeg",
+  {
+    id: 2,
+    location: {
+      city: "Vancouver",
+      country: "Canada",
+    },
+    urls: {
+      full: "/Image-2-Full.jpeg",
+      regular: "/Image-2-Regular.jpeg",
+    },
+    user: {
+      username: "exampleuser2",
+      name: "Jane Example",
+      profile: "/profile-2.jpg",
+    },
   },
-  user: {
-    username: "exampleuser",
-    name: "Joe Example",
-    profile: "/profile-1.jpg",
+  {
+    id: 3,
+    location: {
+      city: "Toronto",
+      country: "Canada",
+    },
+    urls: {
+      full: "/Image-3-Full.jpeg",
+      regular: "/Image-3-Regular.jpeg",
+    },
+    user: {
+      username: "exampleuser3",
+      name: "Sam Example",
+      profile: "/profile-3.jpg",
+    },
   },
-};
+];
 
 const App = () => (
   <div className="App">
-    <PhotoListItem
-      id={sampleDataForPhotoListItem.id}
-      username={sampleDataForPhotoListItem.user.username}
-      imageSource={sampleDataForPhotoListItem.urls.regular}
-      profile={sampleDataForPhotoListItem.user.profile}
-      location={`${sampleDataForPhotoListItem.location.city}, ${sampleDataForPhotoListItem.location.country}`}
-    />
+    {photoData.map((photo) => (
+      <PhotoListItem
+        key={photo.id} // Unique key for React rendering
+        id={photo.id}
+        username={photo.user.username}
+        imageSource={photo.urls.regular}
+        profile={photo.user.profile}
+        location={`${photo.location.city}, ${photo.location.country}`}
+      />
+    ))}
   </div>
 );
 
--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -3,11 +3,11 @@ import "../styles/PhotoListItem.scss";
 
 const PhotoListItem = ({ id, username, imageSource, profile, location }) => (
   <div className="photo-list__item">
-    <img src={imageSource} alt={`Photo ${id}`} className="photo-list__item-image" />
-    <div className="photo-list__item-details">
-      <img src={profile} alt={`${username}'s profile`} className="photo-list__item-profile" />
-      <p className="photo-list__item-username">{username}</p>
-      <p className="photo-list__item-location">{location}</p>
+    <img src={imageSource} alt={`Photo ${id}`} className="photo-list__image" />
+    <div className="photo-list__user-details">
+      <img src={profile} alt={`${username}'s profile`} className="photo-list__user-profile" />
+      <p className="photo-list__user-info">{username}</p>
+      <p className="photo-list__user-location">{location}</p>
     </div>
   </div>
 );
