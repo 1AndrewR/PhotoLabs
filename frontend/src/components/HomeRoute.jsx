a290b95b6f0d3cf8dba4e2d59c4806eec30eb77f
Andrew Roth <a.roth.80@hotmail.com>
2025-03-11 11:43:06 -0700

    using mock data

--- frontend/src/components/HomeRoute.jsx
+++ frontend/src/components/HomeRoute.jsx
@@ -6,7 +6,10 @@ import '../styles/HomeRoute.scss';
 const HomeRoute = ({ photos, topics }) => {
   return (
     <div className="home-route">
+      {/* Render the TopNavigationBar and pass topics */}
       <TopNavigationBar topics={topics} />
+      
+      {/* Render the PhotoList and pass photos */}
       <PhotoList photos={photos} />
     </div>
   );
